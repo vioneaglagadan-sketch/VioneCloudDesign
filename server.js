ef454238c3862d72707321a0dd67e236cca22e97
vioneaglagadan-sketch <vioneaglagadan@gmail.com>
2026-06-05 01:07:47 +0800

    Update server.js

--- server.js
+++ server.js
@@ -4,21 +4,19 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 
-//
 app.get("/", (req, res) => {
     res.send("Backend is working ☁️");
 });
 
-// 
 app.get("/api/cloud-info", (req, res) => {
     res.json({
         project: "Cloud IT Dashboard",
-        status: "Working",
-        message: "Backend is now fixed"
+        status: "Working on Render",
+        message: "API route is now working"
     });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log("Server running on port " + PORT);
